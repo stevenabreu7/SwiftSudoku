25168f6ef438ef449948cc7fe0b4ebb485bf5191
Steven Abreu <stevenabreu7@gmail.com>
2017-05-09 10:52:06 +0200

    + added basic UI

---
+++ Sudoku/Sudoku/Board.swift
@@ -0,0 +1,9 @@
+//
+//  Board.swift
+//  Sudoku
+//
+//  Created by Steven Abreu on 09.05.17.
+//  Copyright © 2017 stevenabreu. All rights reserved.
+//
+
+import Foundation
--- Sudoku/Sudoku/GameScene.swift
+++ Sudoku/Sudoku/GameScene.swift
@@ -11,75 +11,64 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    var textNodes: [[SKLabelNode]] = Array(repeating: Array(repeating: SKLabelNode(), count: 9), count: 9)
+    var buttons: [SKLabelNode] = Array(repeating: SKLabelNode(), count: 9)
+    var touched = (-1,-1)
     
     override func didMove(to view: SKView) {
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
+        let nodeSize = CGSize(width: 0.1111 * self.size.width, height: 0.1111 * self.size.width)
+        let basePosition = CGPoint(x: -0.5 * self.size.width + 0.5 * nodeSize.width, y: 0.5 * self.size.width - 0.5 * nodeSize.height)
+        for i in 0...8 {
+            for j in 0...8 {
+                let node: SKLabelNode = SKLabelNode(fontNamed: "Futura-CondensedMedium")
+                node.text = "0"
+                node.fontSize = 60
+                node.fontColor = UIColor.white
+                node.horizontalAlignmentMode = .center
+                node.position.x = basePosition.x + CGFloat(j) * nodeSize.width
+                node.position.y = basePosition.y - CGFloat(i) * nodeSize.height
+                textNodes[i][j] = node
+                self.addChild(textNodes[i][j])
+            }
         }
-        
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
-    }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
+        for i in 0...8 {
+            let button = SKLabelNode(fontNamed: "Futura-CondensedMedium")
+            button.position.y = -0.4 * self.size.height
+            button.position.x = -0.4 * self.size.width + CGFloat(i) * 0.1 * self.size.width
+            button.fontColor = UIColor.white
+            button.fontSize = 70
+            button.text = String(i+1)
+            buttons[i] = button
+            self.addChild(buttons[i])
         }
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+        for t in touches {
+            let touchedNodes = nodes(at: t.location(in: self))
+            // activate label
+            for i in 0...8 {
+                for j in 0...8 {
+                    if touchedNodes.contains(textNodes[i][j]) {
+                        self.touched = (i,j)
+                        print(i,j)
+                    }
+                }
+            }
+            // select new number
+            for i in 0...8 {
+                if touchedNodes.contains(buttons[i]) {
+                    if self.touched != (-1,-1) {
+                        let (first, second) = self.touched
+                        textNodes[first][second].text = String(i+1)
+                    }
+                }
+            }
+        }
     }
     
     
@@ -87,3 +76,15 @@ class GameScene: SKScene {
         // Called before each frame is rendered
     }
 }
+
+
+// old code:
+//    var shapeNodes: [[SKShapeNode]] = Array(repeating: Array(repeating: SKShapeNode(), count: 9), count: 9)
+//                let shapePosition = CGPoint(x: basePosition.x + CGFloat(j) * nodeSize.width, y: basePosition.y - CGFloat(i) * nodeSize.height)
+//                let shapeNode = SKShapeNode(rect: CGRect(origin: shapePosition, size: nodeSize))
+//                shapeNode.strokeColor = UIColor.lightGray
+//                shapeNode.fillColor = UIColor.darkGray
+//                shapeNodes[i][j] = shapeNode
+//                self.addChild(shapeNodes[i][j])
+//
+//                self.shapeNodes[i][j].addChild(textNodes[i][j])
